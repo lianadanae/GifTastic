45543beaeb92cf6d9f4701e777af4feec551e9fc
lianadanae <lianabette@gmail.com>
2019-09-18 20:39:16 -0400

    fixed buttons to show gifs

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,53 +1,72 @@
 $(document).ready(function () {
 
-  //Array of The Office characters 
-  var topics = ["Michael Scott", "Pam Beesly", "Jim Halpert", "Dwight Schrute", "Kevin Malone",
-    "Angela Martin", "Creed Bratton", "Kelly Kapoor", "Oscar Martinez", "Darryl Philbin",
-  ];
+//Array of The Office characters 
+var topics = ["Michael Scott", "Pam Beesly", "Jim Halpert", "Dwight Schrute", "Kevin Malone",
+  "Angela Martin", "Creed Bratton", "Kelly Kapoor", "Oscar Martinez", "Darryl Philbin",
+];
 
+function renderButtons() {
+  $('#button-group').empty('')
   for (var i = 0; i < topics.length; i++) {
     var button = $("<button>").text(topics[i]);
     button.attr("data-character", topics[i]);
     button.addClass("character-button");
     $("#button-group").append(button);
   }
+}
 
-  // function displayGifs() {
-  $(".character-button").on("click", function (event) {
-    // Find out which button was clicked
-    var character = $(this).attr("data-character");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?&q=" + encodeURI(character) + "&api_key=WRk4IbEQ1U0sBvm8f28l4WGuAeIAjjo7&limit=10";
+$('#add-character-button').on('click', function (event) {
+  event.preventDefault();
+  console.log('this works')
+  var newCharacter = $('#new-character-input').val().trim()
+  topics.push(newCharacter)
+  
+})
+renderButtons();
+$(".character-button").on("click", function () {
+  // Find out which button was clicked
+  var character = $(this).attr("data-character");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?&q=" + encodeURI(character) + "&api_key=WRk4IbEQ1U0sBvm8f28l4WGuAeIAjjo7&limit=10";
 
-    //Setting up the ajax call with parameter {queryURL, method} and making the ajax call
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-      var results = response.data
-      for (var i = 0; i < results.length; i++) {
-        console.log(results[i].images.fixed_height.url);
+  //Setting up the ajax call with parameter {queryURL, method} and making the ajax call
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    var results = response.data
+    for (var i = 0; i < results.length; i++) {
 
-        var gifDiv = $("<div>");
-        var charImage = $("<img>");
+      var gifDiv = $("<div>");
+      var ratingDiv = $("<p>");
+      ratingDiv.text(results[i].rating);
+      var charImage = $("<img>");
 
-        // charImage.attr('src', results[i].images.fixed_height.url);
+      charImage.attr("data-state", "still")
+      charImage.attr('src', results[i].images.original_still.url)
+      charImage.addClass('gifImg')
+      charImage.attr("data-still", results[i].images.original_still.url)
+      //charImage.attr("data-animate", results[i].images.original.url)​
+      $("#gifDiv").append(charImage)
+      $("#gifDiv").append(gifDiv);
 
-
-
-
-
-
-        // var imageDiv = $("<div>");
-        imageDiv.addClass("play");
-        imageDiv.attr("data-state", "still");
-        imageDiv.attr("data-name", topic);
-        imageDiv.attr("data-still", response.data[i].images.fixed_height_still.url);
-        imageDiv.attr("data-animate", response.data[i].images.fixed_height.url)
-
-        $(gifDiv).append(charImage);
-        $(gifDiv).append(charImage);
-        $("#gifDiv").append(gifDiv);
-      }
-    });
+    }
+    
   });
+  
+});
+
+$(document).on("click", ".gifImg", function () {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } 
+  else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+  console.log(state)
+
+});
+
 });
